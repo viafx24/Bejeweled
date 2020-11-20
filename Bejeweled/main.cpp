864e272a792e4a556a405b24ced52bcf4caf17fe
Guillaume Baptist <guillaume.baptist@free.fr>
2020-11-20 11:40:24 +0100

    dernier commit de commentaire: vue global sur le code mais je ne suis pas rentré dans les détails des boucles d'algotihmique

--- Bejeweled/main.cpp
+++ Bejeweled/main.cpp
@@ -1,20 +1,37 @@
+// Jeux sans grand intêrêt, on passera vite.
+// mais a l'air quand même hardu dans le code. regardons Cela.
+// le but est d'aligner trois symboles en hauteur ou largeur qui disparaissent alors
+// et retombe par le haut des nouveaux symboles aleatoires. Il peut y avoir des 
+// evenements en cascade.
+
+// update avant commit final:
+// tout est loin d'être clair mais j'ai l'image global. je bloc là. Seul les points d'algotrithmique necessiterait d'être eclairci.
+// les points de programmation c++ sont clair et c'est cela qui importe.
+
+
 #include <SFML/Graphics.hpp>
 #include <time.h>
 using namespace sf;
 
-int ts = 54; //tile size
-Vector2i offset(48,24);
+int ts = 54; //tile size (des gems)
+Vector2i offset(48,24); // correspond au bord de l'image ajoutée
 
+
+// cette ecriture m'a gené avec le grid[10][10] . Il s'agit bien de la declaration d'une structure de type piece en tableau bidimensionnel.
+// Elle existe certainement en memoire immédiatement (mais je n'ai pas verifié).
 struct piece
 { int x,y,col,row,kind,match,alpha;
   piece(){match=0; alpha=255;}
-} grid[10][10];
+} grid[10][10];// je pense qu'on initialise un tableau bidimensionnel de 10 par 10 piece (comme d'hab vec plus de place car semble 8*8)
 
+
+// on notera ici deux fonctions swap: une qui est la notre , une qui appartient à la std et donc on rajoute le std::
 void swap(piece p1,piece p2)
 {
-  std::swap(p1.col,p2.col);
+  std::swap(p1.col,p2.col);// intervertit les deux objets simplement.
   std::swap(p1.row,p2.row);
 
+  // je n'ai pas regardé précisement le pourquoi mais il faut sans doute réindiqué dans la grille qui est qui.
   grid[p1.row][p1.col]=p1;
   grid[p2.row][p2.col]=p2;
 }
@@ -33,13 +50,18 @@ int main()
 
     Sprite background(t1), gems(t2);
 
+// assez etrange, la colonne et ligne 0 et 9 ne sont pas rempli et l'espèce (kind) est compris entre 0 et 3 or il y a 6 types
+// cela reste coherent dans la mesure ou le tableau est un 8*8
+// il est possible que la taille 10*10 de la grille serve quand on parcourt avec n-1 et n+1 à ne pas depasser la taille du tableau.
+// une astuce à garder en tête.
+
     for (int i=1;i<=8;i++)
      for (int j=1;j<=8;j++)
       {
-          grid[i][j].kind=rand()%3;
-          grid[i][j].col=j;
+          grid[i][j].kind=rand()%3; // bizarre car semble avoir 6 type
+          grid[i][j].col=j;// en colonne (travail avec colonne et ligne commençant à 1 et pas zero)
           grid[i][j].row=i;
-          grid[i][j].x = j*ts;
+          grid[i][j].x = j*ts; // en pixel
           grid[i][j].y = i*ts;
       }
 
@@ -57,35 +79,42 @@ int main()
             if (e.type == Event::MouseButtonPressed)
                 if (e.key.code == Mouse::Left)
                 {
-                   if (!isSwap && !isMoving) click++;
-                   pos = Mouse::getPosition(app)-offset;
+                   if (!isSwap && !isMoving) click++; // ne marche que si on n'est pas en train de bouger ou d'échanger
+                   pos = Mouse::getPosition(app)-offset; // on recupère la position de la souris (soustraction de vecteur j'imagine)
                 }
          }
     
    // mouse click
    if (click==1)
     {
-      x0=pos.x/ts+1;
+      x0=pos.x/ts+1;//enregistre la position du premier click
       y0=pos.y/ts+1;
     }
    if (click==2)
     {
-      x=pos.x/ts+1;
+      x=pos.x/ts+1;// enregistre la position du second click
       y=pos.y/ts+1;
-      if (abs(x-x0)+abs(y-y0)==1)
+
+      // verifie si les deux cases sont adjacentes (horizontalement ou verticalement mais pas diagonalement (2)
+      if (abs(x-x0)+abs(y-y0)==1) 
+          // tente un swap des deux cases en appelant la fonction, reminitialise click et met le booleen isSwap à 1
         {swap(grid[y0][x0],grid[y][x]); isSwap=1; click=0;}
-      else click=1;
+      else click=1;// sinon considère que le deuxième click est un premier click (s'arrêtera dans le if du dessus pour updater x0 et
+      // y0 au temps suivant.
     }
 
    //Match finding
-   for(int i=1;i<=8;i++)
-   for(int j=1;j<=8;j++)
+   // dans les lignes qui suivent, on recherche simplement un match 3 verticalement ou horizontalement 
+   // et on incremente match pour les cellules repondant à la condition.
+
+   for(int i=1;i<=8;i++)// 1 car le -1 qui suit peut donner l'indice 0
+   for(int j=1;j<=8;j++)// idem
    {
     if (grid[i][j].kind==grid[i+1][j].kind)
     if (grid[i][j].kind==grid[i-1][j].kind)
-     for(int n=-1;n<=1;n++) grid[i+n][j].match++;
+     for(int n=-1;n<=1;n++) grid[i+n][j].match++;// si il y a match des lignes, incrémenter match pour les 3 cases en lignes impliquées
 
-    if (grid[i][j].kind==grid[i][j+1].kind)
+    if (grid[i][j].kind==grid[i][j+1].kind)// idem pour les colonnes
     if (grid[i][j].kind==grid[i][j-1].kind)
      for(int n=-1;n<=1;n++) grid[i][j+n].match++;
    }
@@ -106,6 +135,15 @@ int main()
      }
 
    //Deleting amimation
+   // on a l'impression que c'est de la partie dessin mais il n'en est rien.
+   // si on est sur une case avec un match > 0, alors décroit alpha progressivement jusqu'à une transparence trés faible ?
+   // un fois à un alpha de 10, la condition isMoving repasse à false avec la ligne au dessus (au temps suivant)
+
+   // que si n'est pas en train de bouger. en fait juste aprés le swap (mais ca bouge plus)
+   // si une case a un match >1, la transparence alpha diminue progressivement jusqu à 0 (>10 mène à zero je pense)
+   // et on considère que isMoving repasse à true car un effet se produit mais je je comprends alors pas encore pourquoi
+   // on rentr dedans avec le !ismoving
+
    if (!isMoving)
     for (int i=1;i<=8;i++)
     for (int j=1;j<=8;j++)
@@ -115,27 +153,49 @@ int main()
    int score=0;
    for (int i=1;i<=8;i++)
     for (int j=1;j<=8;j++)
-      score+=grid[i][j].match;
+      score+=grid[i][j].match;// le score est simplement l'incrementation de la variable match pour toute les cellules
+   // je me demandais pourquoi match n'était pas un booleen, j'ai ma réponse. Contrairement à ce qu'on pourrait penser
+   // score ne sera pas utilisé pour afficher le score mais réiniatilisé en permanence à zero. il serait facile cependant
+   // d'ajouter le score en créant une deuxième variable sur le coté.
 
    //Second swap if no match
    if (isSwap && !isMoving)
+       // si il y a eu swap mais que il ne doit pas y avoir echange
+       // on verifie si le score est nul et on refait un swap ce qui revient à l'inverser. on repasse le swap à zero
+       // il faudra tester ca en debug
+       // attention, score est réintialisé à chaque temps (il ne montre pas le score à la fin dans la fenètre d'ailleurs)
+
       {if (!score) swap(grid[y0][x0],grid[y][x]); isSwap=0;}
 
    //Update grid
+   // si n'est pas en train de bouger j'imagine
+
    if (!isMoving)
     {
       for(int i=8;i>0;i--)
        for(int j=1;j<=8;j++)
          if (grid[i][j].match)
+
+             // c'est une histoire de: si tu tombe sur un match>0, alors toute les lignes du dessus se decalent vers le bas
+             // et cela n'a lieu qu'avec les lignes car ca marche pareil qu'on est 3 match vertical ou horizontal
+
+             // i correspond à la taille de la ligne: se rappeler que la taille des lignes augmente en descendant d'où le sens inverse du for
          for(int n=i;n>0;n--)
+             //le but, je pense est de faire "remonter" le grid qui match jusqu'à ce qu'il se retrouve en position zero invisible.
+             // cela donne le sentiment avec les swap successif que toutes les cellules sont descendus. Par contre, je ne
+             // comprends pas le break et donc mon explication est peut-être (sans doute) fausse.
             if (!grid[n][j].match) {swap(grid[n][j],grid[i][j]); break;};
+
+      // recréation aléatoire (cette fois on a bien un 7 et pas un trois)
+      // a rebosser.
 
       for(int j=1;j<=8;j++)
        for(int i=8,n=0;i>0;i--)
-         if (grid[i][j].match)
+         if (grid[i][j].match) // pour tout match, il faudra récréer un nouveau symbole
            {
-            grid[i][j].kind = rand()%7;
-            grid[i][j].y = -ts*n++;
+            grid[i][j].kind = rand()%7; // un vrai cette fois (la première fois il n'y en a que trois mais on a pas le temps de le voir)
+            grid[i][j].y = -ts*n++; //le y et c'est ce que j'ai du mal à me mettre dans le crâne correspond au ligne ( et x au colonne) (en général c'est l'inverse)
+            // ici -ts sort de l'écran et c'est effectivement ce qu'on voit pour les nouveaux symboles: il ne depasse qu'une petite partie du bas
             grid[i][j].match=0;
             grid[i][j].alpha = 255;
            }
@@ -148,11 +208,11 @@ int main()
     for (int i=1;i<=8;i++)
      for (int j=1;j<=8;j++)
       {
-        piece p = grid[i][j];
+        piece p = grid[i][j]; // ligne à bosser
         gems.setTextureRect( IntRect(p.kind*49,0,49,49));
         gems.setColor(Color(255,255,255,p.alpha));
         gems.setPosition(p.x,p.y);
-        gems.move(offset.x-ts,offset.y-ts);
+        gems.move(offset.x-ts,offset.y-ts); // ligne à bosser
         app.draw(gems);
       }
 
